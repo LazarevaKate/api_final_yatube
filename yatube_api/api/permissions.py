aa0e7e4f8bcdabe21ac607d155bd9807b803f9d1
LazarevaKate <lazareva.ekaterina2yandex.ru>
2022-04-17 17:32:46 +0300

    Исправлены замечания

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,5 +5,4 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, obj):
         return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-        )
+                or obj.author == request.user)
